d0d648c1120c51d85d988e25283aa00189f5ae6f
lfglfernando <lfglfernandog@gmail.com>
2025-06-04 21:33:24 -0600

    CHANGES

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.BASE_URL}/auth/google/callback`, // <- ESTE CAMPO DEBE COINCIDIR EXACTAMENTE CON GOOGLE CLOUD
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = {
